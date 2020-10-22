9d8b22d48daa6093a3e77d69777f0c949a7fe99a
Soroush Bahrami <sbahrami7@myseneca.ca>
2020-10-21 22:09:13 -0400

    Assignment2-version2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-console.log("meal data: ", dataservice.getAll());
+console.log("meal data: ", dataservice.getOne(1));
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
@@ -36,10 +36,54 @@ app.get("/login", function(req,res){
   res.sendFile(path.join(__dirname,"/views/login.html"));
 });
 
-app.post("/registration", (req,res) =>{
+app.post("/registrationSubmit", (req,res) =>{
+  var bool = true;
+if(!dataservice.validateNull(req.body.FirstName)){
+  res.send({"message": "You must have a first name"})
+bool = false
+}
+if(!dataservice.validateNull(req.body.LastName)){
+  res.send({"message": "You must have a last name"})
+bool = false
+}
+if(!dataservice.validateNull(req.body.emailAddress)){
+  res.send({"message": "You must have a email address"})
+bool = false
+}
+if(!dataservice.validateNull(req.body.passwordInput)){
+  res.send({"message": "You must have a password"})
+bool = false
+}
+if(!dataservice.validatePassword(req.body.passwordInput)){
+ res.send({"message": "invalid password"})
+ bool = false;
+}
+if(bool == true){
   dataservice.register(req.body);
-res.send({"message ":  req.body.nameInput + " was registered"});
+  res.send({"message ":  req.body.FirstName + " " + req.body.LastName + " was registered"})
+}
+});
+
+app.post('/login', (req,res) => {
+  if(dataservice.findEmailAddress(req.body.emailAddress) && dataservice.findpassword(req.body.passwordInput)){
+  res.send({"message": req.body})
+  }else{
+    res.send({"message": "not found"})
+  }
 })
+
+app.get("/getData", function(req,res){
+    
+  var someData = {
+  name: "John",
+  age: 23,
+  occupation: "developer",
+  company: "Scotiabank"
+  };
+
+  res.json(someData);
+});
+
 
 app.use(express.static('public'))
 // setup http server to listen on HTTP_PORT
